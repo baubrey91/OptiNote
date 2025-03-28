1dfb445feef5b5b9f4bc7ebed726b1fb63cde229
Brandon Aubrey <baubrey@paypal.com>
2025-03-28 15:41:57 -0700

    Initial Commit

---
+++ ListBuilder/ListBuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListBuilderApp.swift
+//  ListBuilder
+//
+//  Created by Brandon Aubrey on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct ListBuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ListBuilderTests/ListBuilderTests.swift
@@ -0,0 +1,17 @@
+//
+//  ListBuilderTests.swift
+//  ListBuilderTests
+//
+//  Created by Brandon Aubrey on 3/28/25.
+//
+
+import Testing
+@testable import ListBuilder
+
+struct ListBuilderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
